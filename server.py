5438145375c2a5891581887c564e3335e39ca114
Arnav Sastry <arnavsastry@gmail.com>
2018-03-21 18:55:06 -0500

    init

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/")
+def answer():
+    q = request.args.get("q", "")
+    return "Flask"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
